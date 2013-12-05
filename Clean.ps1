c376d8bdca187e13762011e254942ce091541c55
Kevin Baker <kevin.baker@improvingenterprises.com>
2013-12-05 10:09:33 -0600

    updated for ux preso

--- Clean.ps1
+++
@@ -1,7 +0,0 @@
-param([string] $Path)
-
-# Cleanup any previous execution
-if (Test-Path $Path)
-{
-    Remove-Item -Recurse -Force $Path
-}
--- CleanAll.ps1
+++
@@ -1,2 +0,0 @@
-.\Clean.ps1 BranchingDemo
-.\Clean.ps1 LocalDevelopment
